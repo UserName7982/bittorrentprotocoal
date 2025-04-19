70c9befdd6c7c55dd1a03fcda65be71c1535b084
Harsh <mh116727@gmail.com>
2025-04-19 16:51:19 +0530

    updating file

--- Tracker/Tracker.java
+++ Tracker/Tracker.java
@@ -3,13 +3,25 @@ package Tracker;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+import EncodingFile.endcode;
 import fi.iki.elonen.NanoHTTPD;
 
 public class Tracker extends NanoHTTPD {
+    private static final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
     public ConcurrentHashMap<String, Set<Peer>> peersList = new ConcurrentHashMap<>();
-    private static final int ANNOUNCER_TIMER=30*60*1000;
+    private static final int ANNOUNCER_TIMER = 30 * 60 * 1000;
+    private AtomicInteger leechars = new AtomicInteger(0);
+    private AtomicInteger seeders = new AtomicInteger(0);
+
     public Tracker(int port) {
         super(port);
+        executorService.scheduleAtFixedRate(this::checkInactivePeers, 1, 1, TimeUnit.MINUTES);
     }
 
     public Response serve(IHTTPSession session) {
@@ -31,23 +43,62 @@ public class Tracker extends NanoHTTPD {
         String ip = params.get("ip");
         String key = params.get("key");
         String compact = params.get("compact");
-        peersList.computeIfAbsent(info_hash, k -> ConcurrentHashMap.newKeySet())
-                .add(new Peer(Integer.parseInt(ip), Integer.parseInt(port), Integer.parseInt(peer_id)));
+        boolean isSeeder = "0".equals(left);
+        if ("stopped".equals(event)) {
+            boolean removed = peersList.get(info_hash).removeIf(p -> p.peer_id.equals(peer_id));
+            if (removed) {
+                if (isSeeder)
+                    seeders.decrementAndGet();
+                else
+                    leechars.decrementAndGet();
+            }
+        }
+        boolean b = peersList.computeIfAbsent(info_hash, k -> ConcurrentHashMap.newKeySet())
+                .add(new Peer(ip, Integer.parseInt(port), peer_id));
+        if (b) {
+            if (isSeeder)
+                seeders.incrementAndGet();
+            else
+                leechars.incrementAndGet();
+        }
+        if (event.equals("completed")) {
+            leechars.decrementAndGet();
+            seeders.incrementAndGet();
+        }
+        if (event.equals("uploaded")) {
+            seeders.incrementAndGet();
+        }
+        endcode encoder = new endcode();
+
         return newFixedLengthResponse(Response.Status.OK, "text/plain", "Not Found");
+    }
+
+    public void checkInactivePeers() {
+        AtomicBoolean temp = new AtomicBoolean(false);
+        peersList.forEach((info_hash, peers) -> temp.set(
+                peers.removeIf(
+                        (Peer peer) -> System.currentTimeMillis() - peer.announencetimer.get() > ANNOUNCER_TIMER)));
+        if (temp.get()) {
+            leechars.decrementAndGet();
+        }
     }
 
     public class Peer {
 
-        private int ip;
+        private String ip;
         private int port;
-        private int peer_id;
-        private long announencetimer;
+        private String peer_id;
+        private AtomicLong announencetimer = new AtomicLong();
 
-        public Peer(int ip, int port, int peer_id) {
+        public Peer(String ip, int port, String peer_id) {
             this.ip = ip;
             this.port = port;
             this.peer_id = peer_id;
-            announencetimer = System.currentTimeMillis();
+            Refresh();
+        }
+
+        public void Refresh() {
+            announencetimer.set(System.currentTimeMillis());
         }
     }
 }
